fc86c8238c05af19dc710b8336c807c4cdfac9be
Cristiane Reis <anni.nanne@gmail.com>
2022-06-08 18:52:42 -0300

    Desenvolvendo análise de carteira de ativos

---
+++ carteiraDeAtivos.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import pandas_datareader.data as web
+import matplotlib.pyplot as plt
+
+carteira_ativos = pd_read.excel("nome do arquivo.xlsx")
+print(carteira_ativos)
