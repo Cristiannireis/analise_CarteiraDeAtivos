f6fc6a58c52710677ab18dd614168649dfa72aa6
Cristiane Reis <anni.nanne@gmail.com>
2022-06-09 15:22:35 -0300

    analisando os ativos da carteira

--- carteiraDeAtivos.py
+++ carteiraDeAtivos.py
@@ -3,5 +3,7 @@ import numpy as np
 import pandas_datareader.data as web
 import matplotlib.pyplot as plt
 
-carteira_ativos = pd_read.excel("nome do arquivo.xlsx")
+carteira_ativos = pd.read_excel("ativos.xlsx")
 print(carteira_ativos)
+
+# Criando o dataframe de cotaçoes da carteira
