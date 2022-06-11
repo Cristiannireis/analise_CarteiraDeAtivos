dffaeb480bff381eaf80f1451952b6145cad185e
Cristiane Reis <anni.nanne@gmail.com>
2022-06-10 21:38:46 -0300

    Criando dataframe

--- carteiraDeAtivos.py
+++ carteiraDeAtivos.py
@@ -3,7 +3,13 @@ import numpy as np
 import pandas_datareader.data as web
 import matplotlib.pyplot as plt
 
-carteira_ativos = pd.read_excel("ativos.xlsx")
+carteira_ativos = pd.read_excel("Ativos.xlsx")
 print(carteira_ativos)
 
 # Criando o dataframe de cotaçoes da carteira
+
+cotacoes_carteira_ativos = pd.DataFrame()
+
+for Ativos in carteira_ativos['Ativos']:
+    cotacoes_carteira_ativos[Ativos] = web.DataReader('{}.SA'.format(Ativos), data_source='yahoo', start='2022-01-01',end='2022-06-08')['Adj Close']
+    print(cotacoes_carteira_ativos)
